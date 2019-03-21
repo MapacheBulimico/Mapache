dba1a113ff78914bf0355f5cf09343169f81bfe7
MapacheBulimico <48745623+MapacheBulimico@users.noreply.github.com>
2019-03-21 00:30:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
-const ytdl = require('ytdl-core-discord');
  
 async function play(connection, url) {
   connection.playOpusStream(await ytdl(url));
