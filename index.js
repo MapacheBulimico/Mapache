2b4521e93873b13bd2c54a6dbf37f5eacb93ff36
MapacheBulimico <48745623+MapacheBulimico@users.noreply.github.com>
2019-03-20 22:41:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ client.on("message", (message) => {
     let user = message.mentions.users.first();
     let razon = args.slice(1).join(' ');
     
-    if (message.mentions.users.size < 1) return message.reply('Debesmencionar a alguien.').catch(console.error);
+    if (message.mentions.users.size < 1) return message.reply('Debes mencionar a alguien.').catch(console.error);
     if (!razon) return message.channel.send('Escriba una razón, `-kick @username [razón]`');
     if (!message.guild.member(user).kickable) return message.reply('No puedo patear al usuario mencionado.');
      
