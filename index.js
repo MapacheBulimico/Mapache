3ccbde215fb6314f2248ab57cc05423a90560996
MapacheBulimico <48745623+MapacheBulimico@users.noreply.github.com>
2019-03-21 21:14:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
-var opusscript = require("opusscript");
-
-// 48kHz sampling rate, 20ms frame duration, stereo audio (2 channels)
-var samplingRate = 48000;
-var frameDuration = 20;
-var channels = 2;
-
-// Optimize encoding for audio. Available applications are VOIP, AUDIO, and RESTRICTED_LOWDELAY
-var encoder = new opusscript(samplingRate, channels, opusscript.Application.AUDIO);
-
-var frameSize = samplingRate * frameDuration / 1000;
-
-// Get PCM data from somewhere and encode it into opus
-var pcmData = new Buffer(pcmSource);
-var encodedPacket = encoder.encode(pcmData, frameSize);
-
-// Decode the opus packet back into PCM
-var decodedPacket = encoder.decode(encodedPacket);
-
-// Delete the encoder when finished with it (Emscripten does not automatically call C++ object destructors)
-encoder.delete();
-
-async function play(connection, url) {
-  connection.playOpusStream(await ytdl(url));
-}
- 
-async function play(connection, url) {
-  connection.playOpusStream(await ytdl(url));
-}
-client.on("ready", () => {
-	console.log('El bot esta listo para la musica');
-	client.user.setPresence( {
-       status: "online",
-       game: {
-           name: "--help",
-           type: "PLAYING"
-       }
-   } );
-	});
 
 var prefix = config.prefix;
 
