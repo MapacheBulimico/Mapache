af5edb8a508be9ef4b7301687dd0cc80d6f97579
MapacheBulimico <48745623+MapacheBulimico@users.noreply.github.com>
2019-03-21 21:05:14 -0600

    Update index.js

--- ytdl-core-discord/index.js
+++ ytdl-core-discord/index.js
@@ -1,9 +1,4 @@
-const fs = require('fs');
 const ytdl = require('ytdl-core');
- 
-ytdl('http://www.youtube.com/watch?v=A02s8omM_hI')
-  .pipe(fs.createWriteStream('video.flv'));
-
 const prism = require('prism-media');
 
 function filter(format) {
