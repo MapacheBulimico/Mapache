9c0c74b94c9c70b2e620bf8e04702f9d1d651e8d
MapacheBulimico <48745623+MapacheBulimico@users.noreply.github.com>
2019-03-21 20:38:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,33 @@ const client = new Discord.Client();
 const config = require("./config.json");
 const ffmpeg = require("./node-ffmpeg-binaries-master/index.js");
 const opusscript = require("./opusscript-master/index.js")
-const ytdl = require("./ytdl-core-discord/index.js")
+const ytdl = require('ytdl-core-discord');
+
+var opusscript = require("opusscript");
+
+// 48kHz sampling rate, 20ms frame duration, stereo audio (2 channels)
+var samplingRate = 48000;
+var frameDuration = 20;
+var channels = 2;
+
+// Optimize encoding for audio. Available applications are VOIP, AUDIO, and RESTRICTED_LOWDELAY
+var encoder = new opusscript(samplingRate, channels, opusscript.Application.AUDIO);
+
+var frameSize = samplingRate * frameDuration / 1000;
+
+// Get PCM data from somewhere and encode it into opus
+var pcmData = new Buffer(pcmSource);
+var encodedPacket = encoder.encode(pcmData, frameSize);
+
+// Decode the opus packet back into PCM
+var decodedPacket = encoder.decode(encodedPacket);
+
+// Delete the encoder when finished with it (Emscripten does not automatically call C++ object destructors)
+encoder.delete();
+
+async function play(connection, url) {
+  connection.playOpusStream(await ytdl(url));
+}
  
 async function play(connection, url) {
   connection.playOpusStream(await ytdl(url));
