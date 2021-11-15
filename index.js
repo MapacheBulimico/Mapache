312da8aa6103ec9d6b7d069e3cd0c3536e8e16e8
MapacheBulimico <48745623+MapacheBulimico@users.noreply.github.com>
2021-11-15 16:07:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.on("message", (message) => {
 	if (message.content.startsWith(prefix + "En tiempos de guerra")) {
 		message.channel.send('todo hueco es trinchera. 7u7');
 	}
-	if (message.content.startsWith(prefix + "Adios")) {
+	if (message.content.startsWith(prefix + "adios")) {
 		message.channel.send('Hasta la proxima');
 	}
 	if(message.content.startsWith(prefix + 'help')){
